bbc990c07e34111fae233e20ea8faf0d4df4e000
Bazlur Rashid <mailtobazlur@gmail.com>
2014-09-04 20:06:59 +0600

    drop down fixed

--- app/assets/javascripts/scrips.js
+++ app/assets/javascripts/scrips.js
@@ -4,4 +4,19 @@
 
 $window = $( window ).width();
 
-//alert($window);
+
+$( document ).ready(function() {
+    if($window > 480) {
+        $('.navbar-collapse.collapse li.login').css('display','none');
+    }
+    if($window > 600) {
+        $('.navbar-collapse.collapse li.position').css('display','none');
+    }
+    if($window > 660) {
+        $('.navbar-collapse.collapse li.share').css('display','none');
+    }
+    if($window > 768) {
+        $('.navbar-collapse.collapse li.contact').css('display','none');
+        $('.navbar-collapse.collapse li.help').css('display','none');
+    }
+});
